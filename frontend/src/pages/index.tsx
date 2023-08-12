86e7cbfe53e43f7a37979035fb943fb5fe11d09f
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-08-12 18:31:37 -0400

    wip: add chakra ui

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <main>
+      <p>My next app</p>
+      <Button colorScheme="blue">Button</Button>
+    </main>
+  );
+}
