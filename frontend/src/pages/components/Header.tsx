7f694728550b6c630ba5d52745173748aaa5125d
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-08-25 01:21:05 -0400

    feat: update header
    put some more meat on the skeleton :skull:

--- frontend/src/pages/components/Header.tsx
+++ frontend/src/pages/components/Header.tsx
@@ -1,27 +1,49 @@
-import { Box, Flex, Heading, IconButton } from '@chakra-ui/react';
+import { Box, Flex, HStack, IconButton } from '@chakra-ui/react';
 import { FaBars } from 'react-icons/fa';
+
+type NavLinkProps = {
+  children: React.ReactNode;
+};
+
+const links = ['Home', 'Recipes', 'Pantry'];
+
+const NavLink: React.FC<NavLinkProps> = ({ children }) => {
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded="md"
+      href={'#'}
+      _hover={{
+        textDecoration: 'none',
+        bg: 'gray.200',
+      }}
+      transition={'all 0.2s'}
+    >
+      {children}
+    </Box>
+  );
+};
 
 const Header = () => {
   return (
-    <>
+    <Box bg="gray.100">
       <Flex
         justifyContent="space-between"
         alignItems="center"
-        bg="blue.500"
-        p={4}
+        h={16}
         shadow="md"
+        px={4}
       >
+        <HStack spacing={8} alignItems="center">
+          {links.map((link) => (
+            <NavLink key={link}>{link}</NavLink>
+          ))}
+        </HStack>
         <IconButton aria-label="Menu" icon={<FaBars />} />
-        <Heading color="white">Home</Heading>
-        <Heading color="white" ml={4}>
-          Recipes
-        </Heading>
-        <Heading color="white" ml={4}>
-          Pantry
-        </Heading>
       </Flex>
-      <Box boxSize="40px" bg="white" borderRadius="full" />
-    </>
+    </Box>
   );
 };
 
