8f6270702bb85a8a47570ab3092003e340b361dc
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-09-01 14:23:18 -0400

    expanded recipe card pseudocode

---
+++ frontend/src/components/ExpandedRecipeCard.tsx
@@ -0,0 +1,7 @@
+// this is going to be a lot like the RecipeCard
+// but we'll want to expand on what a recipe will look like
+// both in the database and in the frontend
+
+// we'll want to add a few more fields to the recipe
+// ingredients, an image, instructions, and a category
+// we'll also want to be able to edit the recipe
---
+++ frontend/src/pages/recipes.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+
+export default function Recipes() {
+  return (
+    <main>
+      <Header />
+      <p>This is the recipes page.</p>
+    </main>
+  );
+}
