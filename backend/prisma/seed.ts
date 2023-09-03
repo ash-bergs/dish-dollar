83c85ddd1bd8cc234da725dccf216e59db5cf758
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-09-03 04:59:53 -0400

    feat: basic seeders
    user and ingredients

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,13 +1,21 @@
 // prisma/seed.ts
 import { PrismaClient } from '@prisma/client';
-import seedData from './seeders/restaurants.json';
+import ingredientData from './seeders/ingredients.json';
+import userData from './seeders/users.json';
+
+//TODO: figure out why I couldn't use Faker in here
 
 const prisma = new PrismaClient();
 
 async function main() {
-  for (const restaurant of seedData.restaurants) {
-    await prisma.restaurant.create({
-      data: restaurant,
+  for (const seed of ingredientData.ingredients) {
+    await prisma.ingredient.create({
+      data: seed,
+    });
+  }
+  for (const seed of userData.users) {
+    await prisma.user.create({
+      data: seed,
     });
   }
 }
