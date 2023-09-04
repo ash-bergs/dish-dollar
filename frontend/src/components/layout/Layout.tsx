9edd746647acd43e8cf8ac2f3fbe164ac88b115d
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-09-04 03:31:14 -0400

    feat: add Layout
    now we're not repeating ourselves a bunch to add the header and can easily add the footer, etc, in one place

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/Header';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
+}
+
+export default Layout;
