5f0ddcd7c7aa9b9b82936578fd02706924f30f47
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-09-10 23:01:10 -0400

    chore: comment cleanup

--- frontend/src/components/forms/EditItemForm.pantry.tsx
+++ frontend/src/components/forms/EditItemForm.pantry.tsx
@@ -19,23 +19,13 @@ const EditItemForm = (item: PantryItem): JSX.Element => {
   const itemType = item.type;
   const unitTypeOptions = pantryItemUnits[itemType];
 
-  // we've set up the units object, but we need to create actual options
-  // and map current options to the right shape
+  // map the unit type options to the format that the Select component expects
   const unitOptions = React.useMemo(() => {
     return unitTypeOptions.map((unit) => ({
       value: unit,
       label: unit,
     }));
   }, [unitTypeOptions]);
-
-  // const [currency, setCurrency] = React.useState('USD');
-  // I don't think I'll provide a way to change currency here
-  // the user should set it as a profile option
-  // i see little reason to track different currencies in the same pantry
-  // I do need to provide a way to change the unit
-  // Because a user might have bought a 2L bottle of milk instead of a 1L bottle
-  // but we can still prompt the user for some guidance on how to handle this
-  // ask for metric or imperial at sign up
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- frontend/src/components/inputs/TwoPointNumberInput.tsx
+++ frontend/src/components/inputs/TwoPointNumberInput.tsx
@@ -5,26 +5,20 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
-  useColorModeValue,
   useColorMode,
 } from '@chakra-ui/react';
 import inputStyles from '@/components/inputs/styles';
 
-// created this as a component because I'll probably use this in a lot of places
-// adding recipes
-// updating pantry items
-// adding items to shopping list
-// etc.
-
-// if for some reason I need to have a number input for a different precision
-// then I'll update this to be 'FloatingPointNumberInput' or something
-// and add precision to props
-
+/**
+ * A reusable number input component tailored for two-point precision values.
+ * Use cases include adding recipes, updating pantry items, and adding items to the shopping list.
+ * For different precision needs in the future, consider refactoring or creating a variant.
+ */
 type TwoPointNumberInputProps = {
   defaultValue?: string; // if not passed, we assume the same as value
   onChange: (value: string) => void;
   value: string;
-  format?: string;
+  format?: string; // prepend a format to the value - i.e. '$9.99'
 };
 
 const TwoPointNumberInput = ({
@@ -34,19 +28,16 @@ const TwoPointNumberInput = ({
   format,
 }: TwoPointNumberInputProps): JSX.Element => {
   const initialAmount = defaultValue ? defaultValue : value;
+
   const { colorMode } = useColorMode();
   const styles = inputStyles(colorMode);
-  // If a format is passed it will be prepended to the value
-  // Mostly for currency symbols
-  const formatValue = (val: string) => format + val;
-  const formatedValue = format ? formatValue(value) : value;
 
   return (
     <NumberInput
       {...styles}
       defaultValue={initialAmount}
       onChange={(value) => onChange(value)}
-      value={formatedValue}
+      value={`${format} ${value}`}
       precision={2}
       step={1.0}
       min={0.0}
