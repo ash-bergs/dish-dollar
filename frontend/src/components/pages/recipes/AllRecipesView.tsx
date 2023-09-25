319661cd60495d98532fa57b0611962419fe3a21
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-09-25 00:40:37 -0400

    feat: init page directory, actions bar component

---
+++ frontend/src/components/pages/recipes/AllRecipesView.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useAtom } from "jotai";
+import { RECIPES } from "@/data";
+import { Box, Divider, Flex, Heading } from "@chakra-ui/react";
+import { searchAtom, categoryAtom } from "@/lib/store/recipes.store";
+import ActionsBar from "./ActionsBar";
+import ExpandedRecipeCard from "../../RecipeCard/RecipeCard";
+
+const AllRecipesView = () => {
+  const [searchTerm] = useAtom(searchAtom);
+  const [selectedCategory] = useAtom(categoryAtom);
+
+  // Filtering logic
+  const filteredRecipes = RECIPES.filter((recipe) => {
+    if (selectedCategory && recipe.category !== selectedCategory) {
+      return false;
+    }
+    if (
+      searchTerm &&
+      !recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
+      return false;
+    }
+    return true;
+  });
+
+  return (
+    <Box px={8} py={4}>
+      <Heading>Recipes</Heading>
+      <Divider my={2}/>
+      <ActionsBar />
+      <Flex gap={6} flexWrap={"wrap"}>
+        {filteredRecipes.map((recipe, index) => (
+          <ExpandedRecipeCard key={index} recipe={recipe} />
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default AllRecipesView;
--- frontend/src/pages/recipes.tsx
+++ frontend/src/pages/recipes.tsx
@@ -1,5 +1,5 @@
 import Layout from '@/components/layout/Layout';
-import AllRecipesView from '@/components/AllRecipesView';
+import AllRecipesView from '@/components/pages/recipes/AllRecipesView';
 
 export default function Recipes() {
   return (
