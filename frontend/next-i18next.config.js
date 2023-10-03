c1c39a998c7c43005464f747b53beeaa82d5b013
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-10-03 02:19:21 -0400

    fix: forgot to save these
    🙈

---
+++ frontend/next-i18next.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// setup for internationalization with next-i18next
+// reloadonprerender is set to true in development mode - 
+// reload translations on each page request during prerendering.
+/** @type {import("next-i18next").UserConfig} */
+const config = {
+  debug: process.env.NODE_ENV === 'development',
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'sv'],
+    localeDetection: false,
+  },
+  localePath: path.resolve('./public/locales'),
+};
+
+module.exports = config;
