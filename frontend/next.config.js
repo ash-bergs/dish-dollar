618c419a1589d8826a1f9a7090a8ddb7479f6b38
Ash <65979049+ash-bergs@users.noreply.github.com>
2023-10-03 01:46:21 -0400

    feat: init trpc
    Definitely not done, but we've started!

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { i18n } = require('./next-i18next.config');
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const config = {
+  i18n,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/all',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = config;
---
+++ frontend/src/server/routers/test.ts
@@ -0,0 +1,2 @@
+import { z } from 'zod';
+import { baseProcedure, router } from '../trpc';
